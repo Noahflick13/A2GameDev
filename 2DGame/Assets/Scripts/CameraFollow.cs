a8015a17d91ba126ab2cb406185e6b3b833e25b7
Unknown <noah.f250@stu.nebo.edu>
2018-11-12 10:07:59 -0700

    moar stuff

---
+++ 2DGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public CharacterMove player;
+	public bool isFollowing;
+
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<CharacterMove>();
+
+		isFollowing = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			transform.position = new Vector3(player.transform.position.x + xOffset, player.transform.position.y + yOffset, transform.position.z);
+		}
+		
+	}
+}
